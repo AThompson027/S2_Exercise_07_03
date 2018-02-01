8afcee4ad95951fafa75ef8df251f7456967d4f8
Unknown <AThompson027@west-mec.org>
2018-02-01 15:27:34 -0700

    Global variables & Arrays
    Renamed file, added js file, added name and date, added three global variables that are arrays full of data about days, opponents, and loactions of the games. We also entered a script to call in the tt.js file.

---
+++ tt.js
@@ -0,0 +1,16 @@
+/*
+  JavaScript
+  Exercise 3
+
+  Tipton Turbines
+  Calendar web page
+  Author: Annie Thompson
+  Date: 2.1.18
+
+  Filename: tt.js
+*/
+
+//global variables
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var opponents = ["Lightning", "Combines", "Combines", "Lightning", "Lightning", "Lightning", "Lightning", "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters", "Sodbusters", "(off)", "River Riders", "River Riders", "River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)", "Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines", "Combines"]
