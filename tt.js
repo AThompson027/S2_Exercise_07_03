e5e4daac1d6b679b3b0c0d67c537b2db0982fc9c
Unknown <AThompson027@west-mec.org>
2018-02-01 15:28:05 -0700

    Forgot to stage
    Forgot to stage the rest of the files and changes.

--- tt.js
+++ tt.js
@@ -13,4 +13,15 @@
 //global variables
 var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var opponents = ["Lightning", "Combines", "Combines", "Lightning", "Lightning", "Lightning", "Lightning", "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters", "Sodbusters", "(off)", "River Riders", "River Riders", "River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)", "Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines", "Combines"]
+var opponents = ["Lightning", "Combines", "Combines", "Lightning", "Lightning",
+"Lightning", "Lightning", "Barn Raisers", "Sodbusters", "Sodbusters",
+"Sodbusters", "Sodbusters", "(off)", "River Riders", "River Riders",
+"River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)",
+"Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines",
+"Combines", "(off)", "(off)"];
+
+var gameLocation = ["away", "away", "away", "away", "home", "home",
+"home", "home", "home", "home", "home", "away",
+"away", "away", "away", "", "away", "away",
+"away", "away", "away", "away", "", "home",
+"home", "home", "home", "home", "home", "", ""];
